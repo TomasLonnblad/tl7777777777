0367793235dc800fb30c5d2c73e7107c95d4d48a
Tomas <tomaslonnblad@gmail.com>
2026-01-31 16:40:19 +0100

    Comment out unused imports in page.js
    Comment out unused imports for Image and mountains.

--- src/app/(main)/dashboard/df2/app/page.js
+++ src/app/(main)/dashboard/df2/app/page.js
@@ -1,7 +1,7 @@
 "use client"
-import Image from 'next/image'
+//import Image from 'next/image'
  
-import mountains from '../public/mountains.jpg'
+// import mountains from '../public/mountains.jpg'
  
 
 
